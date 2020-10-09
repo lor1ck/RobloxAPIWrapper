c7c38e198fe1cdb1c20d316f71adbfaa88cffb5b
Vladislav <50027516+lor1ck@users.noreply.github.com>
2020-10-09 21:57:22 +0300

    added new funcs

--- wrapper.py
+++ wrapper.py
@@ -5,8 +5,8 @@ def getUserFollowers(id): return requests.get("https://friends.roblox.com/v1/use
 def getUserFriends(id): return requests.get("https://friends.roblox.com/v1/users/" + str(id) + "/friends").text
 def getUserFriendsCount(id): return requests.get("https://friends.roblox.com/v1/users/" + str(id) + "/friends/count").text
 def getUserFriendsOnline(id): return requests.get("https://friends.roblox.com/v1/users/" + str(id) + "/friends/online").text
-
-
+def getUserFollowedPlaces(id): return requests.get(" https://followings.roblox.com/v2/users/"+ str(id) + "/universes").text
+def getUserCollectibles(id): return requests.get("https://inventory.roblox.com/v1/users/" + str(id) + "//assets/collectibles").text
 
 def friendRequestsDeclineAll(): return requests.post("https://friends.roblox.com/v1/user/friend-requests/decline-all")
 def friendRequestAccept(id): return requests.post("https://friends.roblox.com/v1/users/" + str(id) + "/friend-requests/accept-friend-request")
@@ -15,3 +15,5 @@ def friendUserUnfriend(id): return requests.post("https://friends.roblox.com/v1/
 
 def userFollow(id): return requests.post("https://friends.roblox.com/v1/users/" + str(id) + "/follow")
 def userUnFollow(id): return requests.post("https://friends.roblox.com/v1/users/" + str(id) + "/unfollow")
+def userRequestFriend(id): return requests.post("https://friends.roblox.com/v1/users/" + str(id) + "request-friendship")
+
